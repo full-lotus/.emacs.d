3ba1090d66d0f936539501d31c2507a713cebd3b
Herman Nurlygayanov <shkertik@gmail.com>
2022-01-17 20:34:27 +0500

    Enabled fido-mode

---
+++ init.el
@@ -0,0 +1,17 @@
+(setq package-archives
+ '(("gnu" . "http://elpa.gnu.org/packages/")
+ ("melpa" . "http://melpa.org/packages/")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(leuven))
+ '(fido-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
