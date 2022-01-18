501331c3d4c7c9c5b4bbd6e2fc0bef3a4af27cb3
Herman Nurlygayanov <shkertik@gmail.com>
2022-01-18 13:14:33 +0500

    Bound window switch to M-o

--- init.el
+++ init.el
@@ -2,13 +2,16 @@
  '(("gnu" . "http://elpa.gnu.org/packages/")
  ("melpa" . "http://melpa.org/packages/")))
 
+(global-set-key (kbd "M-o") 'other-window)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(leuven))
- '(fido-mode t))
+ '(fido-mode t)
+ '(winner-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
