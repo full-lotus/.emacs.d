f46f93638b0521083db990f352d23d49e94da254
Herman Nurlygayanov <shkertik@gmail.com>
2023-07-06 20:13:02 +0500

    Added ergo-emacs mode to ease Emacs adoption
    - Having standard shortkeys should help

--- init.el
+++ init.el
@@ -8,16 +8,26 @@
 (global-set-key (kbd "M-[") 'tab-bar-history-back)
 (global-set-key (kbd "M-]") 'tab-bar-history-forward)
 
+(package-initialize)
+(require 'ergoemacs-mode)
+
+(setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
+(setq ergoemacs-keyboard-layout "us") ;; Assumes QWERTY keyboard layout
+(ergoemacs-mode 1)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(custom-enabled-themes '(leuven))
  '(desktop-save t)
  '(desktop-save-mode t)
  '(fido-mode t)
  '(global-tab-line-mode t)
+ '(org-startup-indented t)
+ '(package-selected-packages '(ergoemacs-mode cmake-mode))
  '(tab-bar-history-mode t)
  '(tab-bar-mode t)
  '(winner-mode t))
