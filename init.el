3bcbff5892a01e51f675eb773410a54dedfd0b7e
Herman Nurlygayanov <shkertik@gmail.com>
2023-09-21 08:16:48 +0500

    Add a bunch of stuff from Brave True Clojure setup

--- init.el
+++ init.el
@@ -1,20 +1,107 @@
+;; --------------- package management ---------------
 (setq package-archives
  '(("gnu" . "http://elpa.gnu.org/packages/")
- ("melpa" . "http://melpa.org/packages/")))
+   ("melpa" . "http://melpa.org/packages/")
+   ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
 
+;; setup.el provides a macro for configuration patterns
+;; it makes package installation and config nice and tidy!
+;; https://www.emacswiki.org/emacs/SetupEl
+(if (package-installed-p 'setup)
+    nil
+  (if (memq 'setup package-archive-contents)
+      nil
+    (package-refresh-contents))
+  (package-install 'setup))
+(require 'setup)
+
+
+;; --------------- window/tab navigation ---------------
 (global-set-key (kbd "M-o") 'other-window)
 (windmove-default-keybindings)
 
 (global-set-key (kbd "M-[") 'tab-bar-history-back)
 (global-set-key (kbd "M-]") 'tab-bar-history-forward)
 
-(package-initialize)
+
+;; --------------- ergoemacs, mostly for hotkeys ---------------
 (require 'ergoemacs-mode)
 
 (setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
 (setq ergoemacs-keyboard-layout "us") ;; Assumes QWERTY keyboard layout
 (ergoemacs-mode 1)
 
+
+;; --------------- editing ---------------
+;; Key binding to use "hippie expand" for text autocompletion
+;; http://www.emacswiki.org/emacs/HippieExpand
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+;; Lisp-friendly hippie expand
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
+
+;; Highlights matching parenthesis
+(show-paren-mode 1)
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; line numbers
+(global-display-line-numbers-mode 1)
+;; but not everywhere
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Don't use hard tabs
+(setq-default indent-tabs-mode nil)
+
+;; shell scripts
+(setq-default sh-basic-offset 2
+              sh-indentation 2)
+
+;; When you visit a file, point goes to the last place where it
+;; was when you previously visited the same file.
+;; http://www.emacswiki.org/emacs/SavePlace
+(save-place-mode 1)
+;; keep track of saved places in ~/.emacs.d/places
+(setq save-place-file (concat user-emacs-directory "places"))
+
+;; Emacs can automatically create backup files. This tells Emacs to
+;; put all backups in ~/.emacs.d/backups. More info:
+;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Backup-Files.html
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                               "backups"))))
+(setq auto-save-default nil)
+
+;; comments
+(defun toggle-comment-on-line ()
+  "comment or uncomment current line"
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+(global-set-key (kbd "C-;") 'toggle-comment-on-line)
+
+;; use 2 spaces for tabs
+(defun die-tabs ()
+  (interactive)
+  (set-variable 'tab-width 2)
+  (mark-whole-buffer)
+  (untabify (region-beginning) (region-end))
+  (keyboard-quit))
+
+(setq electric-indent-mode nil)
+
+
+;; --------------- org mode ---------------
 (org-babel-do-load-languages
   'org-babel-load-languages
   '((emacs-lisp . t)
@@ -24,19 +111,81 @@
 ;; For languages with significant whitespace like Python:
 (setq org-src-preserve-indentation t)
 
+
+;; --------------- clojure ---------------
+;; See:  https://clojure-lsp.io/
+;; also: https://emacs-lsp.github.io/lsp-mode/
+(setup (:package lsp-mode lsp-ui lsp-ivy lsp-treemacs)
+  (:hook lsp-enable-which-key-integration)
+  (:bind "M-<f7>" lsp-find-references))
+
+;; clojure-mode is (naturally) the major mode for editing
+;; Clojure and ClojureScript. subword-mode allows words
+;; in camel case to be treated as separate words for
+;; movement and editing commands.
+;; https://github.com/clojure-emacs/clojure-mode
+;; subword-mode is useful for working with camel-case tokens,
+;; like names of Java classes (e.g. JavaClassName)
+(setup (:package clojure-mode)
+  (:hook subword-mode
+         paredit-mode
+         lsp))
+
+;; CIDER is a whole interactive development environment for
+;; Clojure. There is a ton of functionality here, so be sure
+;; to check out the excellent documentation at
+;; https://docs.cider.mx/cider/index.html
+(setup (:package cider)
+  (:bind "C-c u" cider-user-ns
+         "C-M-r" cider-refresh)
+  (:option cider-show-error-buffer t
+           cider-auto-select-error-buffer t
+           cider-repl-history-file "~/.emacs.d/cider-history"
+           cider-repl-pop-to-buffer-on-connect t
+           cider-repl-wrap-history t))
+
+;; company provides auto-completion for CIDER
+;; see https://docs.cider.mx/cider/usage/code_completion.html
+(setup (:package company)
+  (:hook-into cider-mode
+	      cider-repl-mode))
+
+;; hydra provides a nice looking menu for commands
+;; to see what's available, use M-x and the prefix cider-hydra
+;; https://github.com/clojure-emacs/cider-hydra
+(setup (:package cider-hydra)
+  (:hook-into clojure-mode))
+
+;; additional refactorings for CIDER
+;; e.g. add missing libspec, extract function, destructure keys
+;; https://github.com/clojure-emacs/clj-refactor.el
+(setup (:package clj-refactor)
+  (cljr-add-keybindings-with-prefix "C-c C-m")
+  (:hook-into clojure-mode))
+
+;; enable paredit in your REPL
+(setup cider-repl-mode
+  (:hook paredit-mode))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(blink-cursor-mode nil)
- '(custom-enabled-themes '(leuven))
+ '(custom-enabled-themes '(whiteboard))
  '(desktop-save t)
  '(desktop-save-mode t)
  '(display-fill-column-indicator-column 80)
  '(fido-mode t)
  '(global-display-fill-column-indicator-mode t)
  '(global-tab-line-mode t)
+ '(ispell-dictionary nil)
  '(org-startup-indented t)
  '(package-selected-packages '(clojure-mode magit ergoemacs-mode cmake-mode))
  '(tab-bar-history-mode t)
