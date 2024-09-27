27b38df1cafa6b12347d51df1d13c0cf446da5ad
German Nurlygaianov <shkertik@gmail.com>
2024-09-27 18:12:27 +0200

    added ivy + counsel + swiper for better completion/search
    - Actually I wanted a search and replace like in VS Code
    - It seems that the recommended way is to use counsel-rg + occur + wgrep
    - TODO: make search and replace work like in VS Code

--- init.el
+++ init.el
@@ -28,7 +28,7 @@ instead of setq, to avoid confusion in Customize interface"
 
 (custom-set-variables
  '(package-selected-packages
-   '(org-babel-eval-in-repl
+   '(org-babel-eval-in-repl ivy counsel
      ob-clojurescript ob-async async paredit clj-refactor
      openwith org-tidy cider treemacs-all-the-icons treemacs
      clojure-mode magit)))
@@ -37,6 +37,43 @@ instead of setq, to avoid confusion in Customize interface"
 
 
 ;; --------------- general global settings  ---------------
+;; use Ivy + Counsel + Swiper for better completion/search
+;; settings taken from here https://github.com/abo-abo/swiper
+(ivy-mode)
+(counsel-mode)
+;; Enable fuzzy matching
+(setq-and-tell-customize 'ivy-re-builders-alist
+                         '((counsel-M-x . ivy--regex-ignore-order)
+                           (t . ivy--regex-plus)))
+(setq-and-tell-customize 'ivy-use-virtual-buffers t)
+(setq-and-tell-customize 'enable-recursive-minibuffers t)
+;; enter an input that matches one of the candidates instead of this candidate
+(setq-and-tell-customize' ivy-use-selectable-prompt t)
+;; enable this if you want `swiper' to use it
+;; (setq search-default-mode #'char-fold-to-regexp)
+(global-set-key "\C-s" 'swiper)
+;; some of those hotkeys are redundant because of counsel-mode
+;; but I'm not sure which I can drop, and to lazy to check one-by-one
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
+
+
 ;; hide useless title bar, but allow resizing
 (add-to-list 'default-frame-alist '(undecorated . t))
 (add-to-list 'default-frame-alist '(drag-internal-border . 1))
@@ -118,12 +155,12 @@ instead of setq, to avoid confusion in Customize interface"
   '(
     ;; treemacs is a file navigator, typically glued to the left screen side
     ("\\*Treemacs-.*?" (display-buffer-in-direction) (direction . left))
-    
+
     ;; Org Src buffers should pop up in the same window
     ("\\*Org Src.*\\*"
      (display-buffer-same-window
       display-buffer-use-least-recent-window))
-    
+
     ;; open cider-test-report in another window, to keep the relevant tests open
     ("\\*cider-test-report\\*"
      (display-buffer-use-least-recent-window)
