6684ef211a8eb733cc41a7e7e4ce098dc3823825
German Nurlygaianov <shkertik@gmail.com>
2024-10-02 16:31:49 +0200

    rework window navigation due to dropping cua-mode

--- init.el
+++ init.el
@@ -362,10 +362,10 @@ instead of setq, to avoid confusion in Customize interface"
 
 
 ;; --------------- window/tab navigation ---------------
-(global-set-key (kbd "M-o") 'other-window)
+(global-set-key (kbd "C-o") 'other-window)
+(global-set-key (kbd "M-o") 'other-frame)
 (global-set-key (kbd "C-b") 'treemacs)
-(global-set-key (kbd "C-w") 'bury-buffer)
-(global-set-key (kbd "C-o") 'other-frame)
+(global-set-key (kbd "C-x w") 'bury-buffer)
 
 (custom-set-variables
  ;; enable tab-bar-mode
