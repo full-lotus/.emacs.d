fe66a51736e63c1dbeb747f506a5f7e9b3eda576
German Nurlygaianov <shkertik@gmail.com>
2024-11-14 18:44:42 +0100

    enable multiline search in counsel-rg

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(blink-cursor-mode nil)
  '(cider-font-lock-dynamically '(macro function var core))
  '(counsel-rg-base-command
-   '("rg" "--max-columns" "240" "--with-filename" "--no-heading" "--line-number" "--color" "never" "%s"))
+   '("rg" "--multiline" "--multiline-dotall" "--max-columns" "240" "--with-filename" "--no-heading" "--line-number" "--color" "never" "%s"))
  '(custom-enabled-themes nil)
  '(custom-safe-themes
    '("9ab9441566f7c3b059a205a7c5fad32a58422c2695815436d8cc087860b8c2e5" "7613ef56a3aebbec29618a689e47876a72023bbd1b8393efc51c38f5ed3f33d1" "703a3469ae4d2a83fd5648cac0058d57ca215d0fea7541fb852205e4fae94983" "c7a926ad0e1ca4272c90fce2e1ffa7760494083356f6bb6d72481b879afce1f2" "8a0eba8f06120605030161981032469d87a9832402e4441daa91296aa463f124" "42b221449475e41bc04c5663164ffc5d1672c53163630d41d57ef27d5a01cca9" "21055a064d6d673f666baaed35a69519841134829982cbbb76960575f43424db" default))
