0bf10033ec5680ecdcba5255f63e21e6eb877b75
German Nurlygaianov <shkertik@gmail.com>
2025-03-18 16:55:32 +0100

    changed comment font color to less bright to make it readable

--- custom.el
+++ custom.el
@@ -80,7 +80,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:foreground "medium aquamarine"))))
+ '(font-lock-comment-face ((t (:foreground "dark green"))))
  '(font-lock-constant-face ((t (:foreground "saddle brown"))))
  '(font-lock-doc-face ((t (:inherit font-lock-comment-face))))
  '(font-lock-keyword-face ((t (:foreground "violet red"))))
