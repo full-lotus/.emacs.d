522b3176c3bbc92d4c2a3c11cb674ba91d3d0e8f
German Nurlygaianov <shkertik@gmail.com>
2026-02-27 20:36:58 +0100

    added markdown-mode

--- elisp/custom-set-variables.el
+++ elisp/custom-set-variables.el
@@ -26,10 +26,10 @@
 															cider clojure-mode command-log-mode consult corfu
 															dired-ranger embark embark-consult flycheck
 															flycheck-clj-kondo iedit keyfreq magit marginalia
-															ob-async ob-clojurescript openwith orderless
-															pcre2el poly-org posframe puni rainbow-delimiters
-															timeout transient treemacs treemacs-all-the-icons
-															vertico wgrep yasnippet))
+															markdown-mode ob-async ob-clojurescript openwith
+															orderless pcre2el poly-org posframe puni
+															rainbow-delimiters timeout transient treemacs
+															treemacs-all-the-icons vertico wgrep yasnippet))
  '(safe-local-variable-directories
 	 '("/mnt/sda2/projects/dev/kombinacija/code"
 		 "/mnt/sda2/projects/dev/simple-clojure-project/"))
